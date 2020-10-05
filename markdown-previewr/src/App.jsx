0f1f37c2db1475e5b7f1e3960877175b721e4208
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-10-06 00:08:36 +0600

    :heavy_plus_sign: Add github css styles, open new tab on link click

--- markdown-previewr/src/App.jsx
+++ markdown-previewr/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'github-markdown-css/github-markdown.css'
 import Markdown from './pages/Markdown';
 
 function App() {
--- markdown-previewr/src/pages/Markdown.jsx
+++ markdown-previewr/src/pages/Markdown.jsx
@@ -2,9 +2,20 @@ import React, { useEffect, useState } from 'react';
 import marked from "marked";
 import README from './README.md';
 
+marked.setOptions({
+	breaks: true,
+	sanitize: true
+})
+
 function Markdown() {
 	const [editorValue, setEditor] = useState('');
 	const [previewValue, setPreviewValue] = useState(editorValue);
+	const renderer = new marked.Renderer();
+
+	renderer.link = ( href, title, text ) => {
+		return `<a target='_blank' href=${href} title=${title}>${text}</a>`;
+	}
+	marked.use({renderer})
 
 	useEffect(() => {
 		setPreviewValue(marked(editorValue));
@@ -28,7 +39,7 @@ function Markdown() {
 					onChange={(e) => setEditor(e.target.value)}
 				></textarea>
 			</div>
-			<div className='previewer-block'>
+			<div className='previewer-block markdown-body'>
 				<div id='preview'
 				dangerouslySetInnerHTML={{__html: previewValue}}
 				></div>
