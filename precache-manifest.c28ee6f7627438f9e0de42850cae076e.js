6d6a480608638926f63619caa3e0a33731926901
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-10-08 17:37:24 +0600

    Updates

---
+++ precache-manifest.c28ee6f7627438f9e0de42850cae076e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d922ab08626b55b2e494488bf5b3c634",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae0d05fc3de1a2e20f89",
+    "url": "./static/css/main.9ac41148.chunk.css"
+  },
+  {
+    "revision": "81d9b3b19316bdc4e017",
+    "url": "./static/js/2.5398bd0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5398bd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0d05fc3de1a2e20f89",
+    "url": "./static/js/main.3a6d9d8d.chunk.js"
+  },
+  {
+    "revision": "1de6ec5946f991259056",
+    "url": "./static/js/runtime-main.11d4fa65.js"
+  }
+]);
